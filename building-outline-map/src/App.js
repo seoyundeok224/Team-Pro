9f6e1184599cb872fe4f0cb88f1fee1c2d94168e
noiredex <thevertaurore@gmail.com>
2025-07-11 09:18:52 +0900

    맵연동, 브이월드 관련 css, 미사용코드 정리

--- building-outline-map/src/App.js
+++ building-outline-map/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Sidebar from './components/Sidebar/Sidebar';
@@ -7,35 +7,21 @@ import Footer from './components/Footer/Footer';
 import Popup from './components/Popup/Popup';
 import NaverMap from './components/Map/NaverMap';
 
-const ATTRIBUTION = '© Naver Maps, © NAVER Corp.';
-
 function App() {
   const [showEmoji, setShowEmoji] = useState(true);
   const [mapStyle, setMapStyle] = useState('base');
   const [darkMode, setDarkMode] = useState(false);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // 로그인 상태 관리
 
-
+ 
   const [searchQuery, setSearchQuery] = useState('');
-  const [markerPosition, setMarkerPosition] = useState(null);
 
 
-// 다크모드
   useEffect(() => {
     document.body.style.backgroundColor = darkMode ? '#222' : '#fff';
     document.body.style.color = darkMode ? '#fff' : '#000';
   }, [darkMode]);
 
-//    useEffect(()=>{
-//     const initMap = () => {
-//       const map = new naver.maps.Map("map", {
-//         center: new naver.maps.LatLng(37.5665, 126.9780), // 초기 위치
-//         zoom: 18,
-//       });
-//       setMap(map);
-//     }
-//   initMap();
-// },[]);
 
   return (
     <div className={`App ${darkMode ? 'dark' : ''}`}>
@@ -55,16 +41,13 @@ function App() {
           setSearchQuery={setSearchQuery}
         />
 
-        <div className="map_container">
-          <NaverMap
-            markerPosition={markerPosition}
-            setMarkerPosition={setMarkerPosition}
-          />
+        <div className="map-container">
+          <NaverMap />
         </div>
       </div>
       <Footer darkMode={darkMode} />
     </div>
   );
-};
+}
 
 export default App;
